9098a8bd30cfe11fa9ff327c98e96a7fb8f2deb9
DachiGoshadze <113930769+DachiGoshadze@users.noreply.github.com>
2025-06-14 19:05:19 +0400

    add base log for mail sender service

--- Infrastructure.MailSenderService/Implementations/MailService.cs
+++ Infrastructure.MailSenderService/Implementations/MailService.cs
@@ -2,10 +2,11 @@ using System.Net;
 using System.Net.Mail;
 using Core.Application.Interfaces.Services;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace infrastructure.MailSenderService.Implementations;
 
-public class MailService(IConfiguration configuration) : IMailService
+public class MailService(IConfiguration configuration, ILogger<IMailService> _logger) : IMailService
 {
     public async Task<bool> SendAuthMail(string to, string subject, string body)
     {
@@ -42,6 +43,7 @@ public class MailService(IConfiguration configuration) : IMailService
         }
         catch (Exception ex)
         {
+            _logger.LogInformation("Failed to send email to {Email} with subject {Subject}. Exception : {ex}", to, subject, ex);
             return false;
         }
     }
